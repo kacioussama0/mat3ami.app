3a4c579f4c58287137159b645f2a9cdff4d0ba3b
KACI OUSSAMA <kacioussama0@gmail.com>
2026-02-01 15:35:31 +0100

    add-tables-model-migration

---
+++ database/migrations/2026_02_01_143058_create_tables_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tables', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',32);
+            $table->unsignedInteger('capacity')->nullable();
+            $table->enum('status', ['free','occupied','reserved'])->default('free');
+            $table->boolean('is_active')->default(true);
+            $table->foreignId('zone_id')->nullable()->constrained('zones');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->unique(['restaurant_id', 'name']);
+            $table->index(['restaurant_id', 'status']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tables');
+    }
+};
